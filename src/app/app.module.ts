20e5930fcfa17eb99006795ff84adb3bb773226f
Tikerin <dimakrylyuk@gmail.com>
2018-11-02 16:22:52 +0200

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
+import 'hammerjs';
+
+// decorators it's function which modify JS classes 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent              // declare that this view Class belong to this particular module
   ],
-  imports: [
+  imports: [                  //need to be used with AppModule
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],              // specify which modules it will use
+  bootstrap: [AppComponent]   // bootstrap AppComponent(root component)
 })
 export class AppModule { }

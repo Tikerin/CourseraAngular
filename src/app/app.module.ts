a5d942b8aa49bd89ca925e1e11377a5a1942f9e5
Tikerin <dimakrylyuk@gmail.com>
2018-11-02 19:18:26 +0200

    images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+
 import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 // decorators it's function which modify JS classes 
 @NgModule({
   declarations: [
-    AppComponent              // declare that this view Class belong to this particular module
+    AppComponent,
+    MenuComponent              // declare that this view Class belong to this particular module
   ],
   imports: [                  //need to be used with AppModule
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],              // specify which modules it will use
   bootstrap: [AppComponent]   // bootstrap AppComponent(root component)

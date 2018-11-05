6e43deff3008417dc91078e5a9e1ec21c71068d9
Tikerin <dimakrylyuk@gmail.com>
2018-11-05 21:26:39 +0200

    Add spiner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import {FlexLayoutModule} from '@angular/flex-layout';
-
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list'
-
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
@@ -25,6 +16,25 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; 
 
 // decorators it's function which modify JS classes 
 @NgModule({
@@ -36,21 +46,34 @@ import { PromotionService } from './services/promotion.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent              // declare that this view Class belong to this particular module
+    ContactComponent,
+    LoginComponent              // declare that this view Class belong to this particular module
   ],
   imports: [                  //need to be used with AppModule
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
 
+    MatProgressSpinnerModule,
     MatToolbarModule,    
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [DishService,PromotionService],              // specify which modules it will use
+  entryComponents: [
+    LoginComponent
+  ] ,
+  providers: [DishService,PromotionService,LeaderService],              // specify which modules it will use
   bootstrap: [AppComponent]   // bootstrap AppComponent(root component)
 })
 export class AppModule { }
---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Leader } from '../shared/Leader';
+import { LEADERS } from '../shared/Leaders';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  constructor() { }
+
+  getLeaders(): Promise<Leader[]> {
+    return Promise.resolve(LEADERS);
+  }
+  
+  getLeader(id: string): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+  }
+
+  getFeaturedLeader(): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+  }
+
+}
